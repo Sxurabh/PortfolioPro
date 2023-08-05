a47c155f8d2e522045dd767a35c460cca08249bf
Saurabh Kirve <>
2023-08-05 23:51:34 +0530

    Initial push

---
+++ SaurabhPortfolio/assets/js/script.js
@@ -0,0 +1,152 @@
+//Menu
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+
+//Maquina de escrever
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 100 * i);
+  });
+}
+const titulo = document.querySelector(".maquina-escrever");
+typeWrite(titulo);
+
+//Scroll TOP
+$(document).ready(function () {
+  var scrollTop = $(".scrollTop");
+
+  $(window).scroll(function () {
+    var topPos = $(this).scrollTop();
+    //Quantidade Y que o botão de scroll vai aparecer
+    if (topPos > 500) {
+      $(scrollTop).css("opacity", "1");
+    } else {
+      $(scrollTop).css("opacity", "0");
+    }
+  });
+
+  //Velocidade a animação
+  $(scrollTop).click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      100
+    );
+    return false;
+  });
+});
+
+//Revelar animação
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2000,
+  reset: true,
+});
+
+//Home delay
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".linguagensProjeto", { delay: 400 });
+sr.reveal(".imagem-espaco-delay", { delay: 200 });
+sr.reveal(".imagem-iuri-delay", { delay: 600 });
+sr.reveal(".scroll-delay", { delay: 1000 });
+//Sobre mim delay
+sr.reveal(".imagem-delay", {});
+sr.reveal(".titulo-delay", { delay: 200 });
+sr.reveal(".descricao-delay", { delay: 400 });
+sr.reveal(".cards-interval", { interval: 400 });
+
+//Animação conhecimentos
+var texto1 = document.querySelector(".mudarTextoDescricao");
+
+document.querySelector(".html").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "HTML is a markup-based language, where we mark up elements to define what information the page will display. <br><br>";
+});
+
+document.querySelector(".css").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "CSS is a “layered” style sheet language created for the purpose of styling HTML pages. <br><br> ";
+});
+
+document.querySelector(".js").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "JavaScript is a structured, high-level scripting interpreted programming language with weak dynamic typing and multiparadigm. <br><br> ";
+});
+
+document.querySelector(".tailwind").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "Tailwind CSS is a framework designed to maximize the potential of good old CSS and take it even further. <br><br>";
+});
+
+document.querySelector(".sass").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+  texto1.innerHTML =
+    "React is an open source JavaScript library focused on creating user interfaces on web pages. <br><br> ";
+});
+
+document.querySelector(".next").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "Next.js is a web framework that enables functionality such as server-side rendering and static web site generation based on React. <br><br> ";
+});
+
+document
+  .querySelector(".styled")
+  .addEventListener("mouseover", function mudarTexto8() {
+    texto1.innerHTML =
+      "Styled Components is a library that uses the concept of CSS-in-JS, that is, it allows us to write CSS codes within Javascript. <br><br> ";
+  });
+
+var sairCaixa = document.getElementsByClassName("sairCaixa");
+
+for (i = 0; i < sairCaixa.length; i++) {
+  sairCaixa[i].addEventListener("mouseout", function mudarTextoNormal() {
+    texto1.innerHTML = `I am convinced that together we will achieve incredible things. 💜<br><br>*mouse over card to read about*`;
+  });
+}
+
