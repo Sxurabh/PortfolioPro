f11f379bce334e39a3a1d9d0d08480b15f724718
Saurabh Kirve <>
2023-09-04 03:48:26 +0530

    dark mode button added

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -150,3 +150,34 @@ for (i = 0; i < sairCaixa.length; i++) {
   });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggleButton = document.getElementById("theme-toggle-button");
+  const toggleInput = document.getElementById("toggle");
+
+  // Retrieve user preference from localStorage
+  const storedThemePreference = localStorage.getItem("darkMode");
+
+  // Apply initial theme based on user preference
+  const isDarkModePreferred = storedThemePreference === "true";
+  applyTheme(isDarkModePreferred);
+  toggleInput.checked = isDarkModePreferred;
+
+  // Toggle theme on button click
+  themeToggleButton.addEventListener("click", () => {
+    const isDarkMode = toggleInput.checked;
+    applyTheme(isDarkMode);
+
+    // Store user preference in localStorage
+    localStorage.setItem("darkMode", isDarkMode);
+  });
+
+  function applyTheme(isDarkMode) {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+      document.body.classList.remove("light-mode");
+    } else {
+      document.body.classList.add("light-mode");
+      document.body.classList.remove("dark-mode");
+    }
+  }
+});
